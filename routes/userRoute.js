c03601fb26cc806389f0f4833347836569eba517
shahnoor-oneRoot <shahnoor@oneroot.farm>
2025-03-19 15:53:07 +0530

    changed the temp controller made for userconsent overwritting

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -4,13 +4,14 @@ import { Router } from "express";
 import {
   concentAddAllowOverwrite,
   importCsv,
+  concentAdd,
 } from "../controller/userController.js";
 
 const route = Router();
 const upload = multer({ dest: "uploads/" });
 
 // route.put("/consent", concentAdd);
-route.put("/consent", upload.single("csv"), concentAddAllowOverwrite);
+route.put("/consent", upload.single("csv"), concentAdd);
 route.post("/user-import", upload.single("csv"), importCsv);
 
 export default route;
