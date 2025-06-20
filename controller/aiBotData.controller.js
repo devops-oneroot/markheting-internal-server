1868e8845d5459d76e03c498888860a1f162b869
HaiderNadaf <haidernadaf67@gmail.com>
2025-06-20 18:12:38 +0530

    create aibotData delete api

--- controller/aiBotData.controller.js
+++ controller/aiBotData.controller.js
@@ -83,3 +83,25 @@ export const getUser = async(req,res) => {
   res.status(200).json({data})
 
 }
+
+export const deleteUser = async (req, res) => {
+  try {
+    const { number } = req.query;
+
+    if (!number) {
+      return res.status(400).json({ message: "Number is required" });
+    }
+
+    const user = await aibotData.findOneAndDelete({ number });
+
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    res.status(200).json({ message: "User deleted successfully", user });
+  } catch (error) {
+    console.error("Delete Error:", error.message);
+    res.status(500).json({ message: "Internal server error", error: error.message });
+  }
+};
+
