e1e02ee7f98fc37cf3c7cea1b16487fb0eab81ad
HaiderNadaf <haidernadaf67@gmail.com>
2025-07-18 17:21:07 +0530

    fixed:cors error (was comeneted)

--- server.js
+++ server.js
@@ -27,15 +27,8 @@ async function startServer() {
     const app = express();
     app.use(express.json());
     app.use(express.urlencoded({ extended: true }));
-    // app.use(cors());
+    app.use(cors());
 
-    app.use(
-      cors({
-        origin: "http://localhost:3000",
-        methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-        credentials: true,
-      })
-    );
     // Routes
     app.use(userRoute);
     app.use(plivoRoute);
